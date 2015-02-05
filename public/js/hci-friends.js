63315cddcba823fd1a0e4a9ff505444ae62b9a6b
Sachi Pitkin <spitkin@ucsd.edu>
2015-02-05 22:53:03 +0000

    lab5

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -5,12 +5,22 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	
 }
+
+$('.f').click(happening);
+function happening(event){
+	event.preventDefault();
+	$(this).text(anagrammedName($(this).text()));
+}
+
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,16 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	console.log("reach");
+	var newName = req.query.name;
+    var desc = req.query.description;
+    var img = 'http://lorempixel.com/400/400/people';
+
+    var  newFriend = {
+    				"name": newName,
+    				"description": desc,
+    				"imageURL": img
+    			};
+	data["friends"].push(newFriend);
+	res.render('add');
  }
